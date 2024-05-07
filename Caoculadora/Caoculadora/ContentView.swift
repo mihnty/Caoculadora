116dedf3e3c0f6de08d86644715cd90a0ded1a71
mih <naomiklin@gmail.com>
2024-05-07 17:39:12 -0300

    Implementa TextFields

--- Caoculadora/Caoculadora/ContentView.swift
+++ Caoculadora/Caoculadora/ContentView.swift
@@ -8,35 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var years: Int? = nil
+    @State var months: Int? = nil
+
+    
     var body: some View {
         VStack (spacing: 24) {
             ZStack {
                 Rectangle()
                     .ignoresSafeArea(.all)
                     .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                    .frame(height: 120)
+                    .frame(height: 140)
                 HStack (spacing: 16) {
-                    Image(systemName: "dog.circle")
+                    Image(systemName: "pawprint.circle")
                         .resizable()
                         .frame(width: 40, height: 40)
                         .foregroundColor(.white)
-                        .padding(.top, 40)
+                        .padding(.top, 60)
                     Text("Cãoculadora")
                         .font(.title)
                         .fontWeight(.heavy)
                         .foregroundColor(Color.white)
-                        .padding(.top, 40)
+                        .padding(.top, 60)
                     Spacer()
                 }
                 .padding(20)
             }
-            Text("AuAuAuAu!")
-                .font(.largeTitle)
-                .fontWeight(.bold)
+                VStack(alignment: .leading, spacing: 32) {
+                    Text("*Samuel* tem: \(Double(years ?? 0) + Double(months ?? 0)/12) anos, um idoso!")
+                    Text("Qual a idade do seu cão?")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                    VStack (alignment: .leading, spacing: 8) {
+                        Text("Anos")
+                            .font(.headline)
+                        TextField("", value: $years, format: .number)
+                    }
+                    VStack (alignment: .leading, spacing: 8) {
+                        Text("Meses")
+                            .font(.headline)
+                        TextField("Digite os meses aqui", value: $months, format: .number)
+                }
+            }
+            .textFieldStyle(.roundedBorder)
+            .keyboardType(.numberPad)
+            .padding()
             HStack {
                 Image(systemName: "dog.fill")
                     .resizable()
-                    .frame(width: 100, height: 88)
+                    .frame(width: 80, height: 72)
                     .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
                 Image(systemName: "heart")
                     .resizable()
@@ -44,34 +64,17 @@ struct ContentView: View {
                     .foregroundColor(.blue)
                 Image(systemName: "dog.fill")
                     .resizable()
-                    .frame(width: 100, height: 88)
+                    .frame(width: 80, height: 72)
                     .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
                     .scaleEffect(x: -1, y: 1)
             }
-            Text("*Bark Bark*")
-            VStack {
-                HStack {
-                    Text("*Woof woof*:")
-                        .font(.headline)
-                        .multilineTextAlignment(.leading)
-                    Spacer()
-                        .frame(height: 24)
-                }
-                .padding(.leading,20)
-                RoundedRectangle(cornerRadius: 5)
-                    .stroke(.blue, lineWidth: 2)
-                    .frame(width: 344, height: 40)
-                    .foregroundColor(.white)
-                Spacer()
-                    .frame(height: 24)
-            }
             Image(.doggo)
                 .resizable()
-                .frame(width: 200, height: 200)
+                .frame(width: 160, height: 160)
                 .cornerRadius(20)
             ZStack {
                 Capsule()
-                    .frame(width: 160, height: 48)
+                    .frame(width: 160, height: 32)
                     .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
                 Text("GROWL")
                     .font(.headline)
