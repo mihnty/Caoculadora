4a90b16f4e0ff048a0dfe99de9d58e93577068e5
mih <naomiklin@gmail.com>
2024-05-08 18:03:35 -0300

    Adicionado botão e imagem

--- Caoculadora/Caoculadora/ContentView.swift
+++ Caoculadora/Caoculadora/ContentView.swift
@@ -10,80 +10,83 @@ import SwiftUI
 struct ContentView: View {
     @State var years: Int? = nil
     @State var months: Int? = nil
-
+    @State var result: Int?
+    
+    private var color: Color = .init(red: 0x4F/0xFF, green: 0x46/0xFF, blue: 0xE5/0xFF)
     
     var body: some View {
         VStack (spacing: 24) {
             ZStack {
                 Rectangle()
                     .ignoresSafeArea(.all)
-                    .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                    .frame(height: 140)
+                    .foregroundColor(color)
+                    .frame(height: 120)
                 HStack (spacing: 16) {
                     Image(systemName: "pawprint.circle")
                         .resizable()
                         .frame(width: 40, height: 40)
                         .foregroundColor(.white)
-                        .padding(.top, 60)
+                        .padding(.top, 48)
                     Text("Cãoculadora")
                         .font(.title)
                         .fontWeight(.heavy)
                         .foregroundColor(Color.white)
-                        .padding(.top, 60)
+                        .padding(.top, 48)
                     Spacer()
                 }
-                .padding(20)
+                .padding(.horizontal, 20)
             }
-                VStack(alignment: .leading, spacing: 32) {
-                    Text("*Samuel* tem: \(Double(years ?? 0) + Double(months ?? 0)/12) anos, um idoso!")
-                    Text("Qual a idade do seu cão?")
-                        .font(.title2)
-                        .fontWeight(.bold)
-                    VStack (alignment: .leading, spacing: 8) {
-                        Text("Anos")
-                            .font(.headline)
-                        TextField("", value: $years, format: .number)
-                    }
-                    VStack (alignment: .leading, spacing: 8) {
-                        Text("Meses")
-                            .font(.headline)
-                        TextField("Digite os meses aqui", value: $months, format: .number)
+            VStack(alignment: .leading, spacing: 32) {
+                Text("Qual a idade do seu cão?")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .foregroundColor(color)
+                VStack (alignment: .leading, spacing: 8) {
+                    Text("Anos")
+                        .font(.headline)
+                    TextField("Digite os anos completos aqui", value: $years, format: .number)
+                }
+                VStack (alignment: .leading, spacing: 8) {
+                    Text("Meses")
+                        .font(.headline)
+                    TextField("Digite os meses aqui", value: $months, format: .number)
+                }
+                VStack (alignment: .leading, spacing: 8) {
+                    Text("Porte")
+                        .font(.headline)
+                    
                 }
             }
-            .textFieldStyle(.roundedBorder)
-            .keyboardType(.numberPad)
-            .padding()
-            HStack {
-                Image(systemName: "dog.fill")
+                .textFieldStyle(.roundedBorder)
+                .foregroundColor(color)
+                .keyboardType(.numberPad)
+                .padding()
+            if let result {
+                Text("Seu resultado foi:")
+                Text("\(result)!")
+            } else {
+                Image(.doggo)
                     .resizable()
-                    .frame(width: 80, height: 72)
-                    .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                Image(systemName: "heart")
-                    .resizable()
-                    .frame(width: 32, height: 30)
-                    .foregroundColor(.blue)
-                Image(systemName: "dog.fill")
-                    .resizable()
-                    .frame(width: 80, height: 72)
-                    .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                    .scaleEffect(x: -1, y: 1)
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 198.4, height: 150)
+                    .cornerRadius(20)
+                    .padding(.vertical, 40)
             }
-            Image(.doggo)
-                .resizable()
-                .frame(width: 160, height: 160)
-                .cornerRadius(20)
-            ZStack {
-                Capsule()
-                    .frame(width: 160, height: 32)
-                    .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                Text("GROWL")
-                    .font(.headline)
-                    .fontWeight(.bold)
+            Button(action: {
+                result = 23
+            }, label: {
+                Text("Cãocular")
+                    .font(.title3)
+                    .fontWeight(.semibold)
                     .foregroundColor(.white)
+                })
+                .padding()
+                .frame(width: 345, height: 52)
+                .background(color)
+                .cornerRadius(12)
             }
         }
     }
-}
 #Preview {
     ContentView()
 }
